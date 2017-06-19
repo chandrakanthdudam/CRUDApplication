a58800cb2f6535cfd0a9ac4999f48b0bfdcb6fdd
Aaron Long <aaron.long.c@gmail.com>
2017-06-18 22:43:08 -0400

    Everything is broken

--- src/test/java/io/zipcoder/crudapp/PeopleControllerTest.java
+++ src/test/java/io/zipcoder/crudapp/PeopleControllerTest.java
@@ -1,16 +1,23 @@
 package io.zipcoder.crudapp;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.mockito.BDDMockito.given;
@@ -26,17 +33,26 @@ import static org.junit.Assert.assertEquals;
  */
 @RunWith(SpringRunner.class)
 @WebMvcTest(PeopleController.class)
+@ContextConfiguration(classes = { WebConfig.class })
 public class PeopleControllerTest {
 
-  @Autowired
   private MockMvc mvc;
 
   @MockBean
   PersonRepository repository;
 
+  @InjectMocks
+  private PeopleController peopleController;
+
+  @Before
+  public void setupMock() {
+    MockitoAnnotations.initMocks(this);
+    mvc = MockMvcBuilders.standaloneSetup((PeopleController.class)).build();
+  }
+
   @Test
   public void exampleTest() throws Exception {
-    this.mvc.perform(get("/person").accept(MediaType.APPLICATION_JSON))
+    this.mvc.perform(get("/person/").accept(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
             .andExpect(content().string(containsString("[]")));
   }
