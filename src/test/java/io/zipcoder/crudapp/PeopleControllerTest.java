178c9dce917a2d03ccbcf2df991d84ee64303224
Aaron Long <aaron.long.c@gmail.com>
2017-06-19 12:28:36 -0400

    Finally got test working

--- src/test/java/io/zipcoder/crudapp/PeopleControllerTest.java
+++ src/test/java/io/zipcoder/crudapp/PeopleControllerTest.java
@@ -3,66 +3,59 @@ package io.zipcoder.crudapp;
 import java.util.List;
 import java.util.Arrays;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import javax.persistence.PersistenceProperty;
 
 import static org.hamcrest.Matchers.containsString;
-import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import static org.junit.Assert.assertEquals;
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.*;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 /**
  * Created by aaronlong on 6/16/17.
  */
 @RunWith(SpringRunner.class)
-@WebMvcTest(PeopleController.class)
 public class PeopleControllerTest {
-  @Autowired
+
   private MockMvc mvc;
 
   @MockBean
   private PersonRepository repository;
 
-  @MockBean
+  @InjectMocks
   private PeopleController controller;
 
   @Before
   public void init() {
     // Read Up On
     MockitoAnnotations.initMocks(this);
+    mvc = MockMvcBuilders.standaloneSetup(controller)
+                      .build();
+  }
+
+  private String objectToString(List<Person> p) {
+    ObjectMapper mapper = new ObjectMapper();
+    try {
+      return mapper.writeValueAsString(p);
+    } catch (JsonProcessingException e)  {
+      System.out.println(e);
+      return null;
+    }
   }
 
   @Test
@@ -71,10 +64,10 @@ public class PeopleControllerTest {
             new Person( "Daenerys Targaryen", 2),
             new Person( "John Snow", 2));
 
-    //when(controller.getPeople()).thenReturn(users);
-
+    when(repository.findAll()).thenReturn(users);
+    String expected = objectToString(users);
     this.mvc.perform(get("/person/"))
                  .andExpect(status().isOk())
-                 .andExpect(content().string(containsString("[]")));
+                 .andExpect(content().json(expected));
   }
 }
